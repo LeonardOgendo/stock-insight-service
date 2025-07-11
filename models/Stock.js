79610bea94be88aa727eea1d7169bcc77de2f5f9
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-11 14:41:06 +0300

    Remove legacy structure

--- models/Stock.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const stockSchema = new mongoose.Schema({
-  symbol: { type: String, required: true },
-  ips: [String] // store hashed or raw IPs (for FCC we'll start raw)
-});
-
-module.exports = mongoose.model('Stock', stockSchema);
